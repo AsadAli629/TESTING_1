48a9dacd3e97b78eadbc01fa958438b1ef2b0dc5
AsadAli629 <141374317+AsadAli629@users.noreply.github.com>
2024-06-04 14:28:49 +0500

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $("button").click(function () {
+        $(".body").toggleClass("change-background");
+        // $("button").toggleClass("change-b");
+    });
+});
